72fdd16ecea2ae5ec188abe4e761ce5a3963a76d
Nam Yooji <57939150+namyj@users.noreply.github.com>
2023-04-28 14:26:16 +0900

    [Bronze II] Title: 숫자, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.java
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.io.*;
 
 /**
  * 입력 값 조건
@@ -8,17 +8,22 @@ import java.util.Scanner;
  * - 14 8 같이 앞에 수가 뒤에 수보다 작을 때
  */
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int aNum = sc.nextInt();
-        int bNum = sc.nextInt();
-        int cnt = Math.abs(bNum - aNum) - 1;
+    public static void main(String[] args) throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        OutputStreamWriter ow = new OutputStreamWriter(System.out);
+        BufferedWriter bw = new BufferedWriter(ow);
 
-        if (cnt < 0) cnt = 0; // 두 수가 동일할 때
+        String[] iNum = br.readLine().split(" ");
+        int cnt = Math.abs(Integer.parseInt(iNum[1]) - Integer.parseInt(iNum[0])) - 1;
+        if (cnt < 0) cnt = 0;
 
-        System.out.println(cnt); 
+        System.out.println(cnt);
         for (int i = 0 ; i < cnt; i++) {
-            System.out.printf("%d ", Math.min(aNum, bNum) + i + 1);
+            bw.write(String.valueOf(Math.min(Integer.parseInt(iNum[1]), Integer.parseInt(iNum[0])) + i + 1));
+            bw.write(" ");
         }
+        bw.flush();
+        bw.close();
     }
 }
