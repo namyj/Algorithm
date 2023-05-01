3294ee84554f46ef64d2c3cffc90f1f811c5b93f
Nam Yooji <57939150+namyj@users.noreply.github.com>
2023-05-01 11:41:27 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,15 @@
+class Solution {
+     public int solution(int a, int b, int c) {
+        int answer = a + b + c;
+
+        if (a == b || b == c || c == a) {
+            answer *= (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+        }
+
+        if (a == b && b == c && c == a) {
+            answer *= (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+        }
+
+        return answer;
+    }
+}
