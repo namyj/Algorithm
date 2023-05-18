e8ccdb1348b84c0c0df6acbe1952d9cfdff78901
Nam Yooji <57939150+namyj@users.noreply.github.com>
2023-05-18 09:53:49 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 912 ms, Memory: 111644 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] numList = new int[2000001];
+        
+        // c <= 100,0000 and c >= -100,0000  
+        for (int i=0; i < n; i++) {
+            int c = Integer.parseInt(br.readLine());
+            numList[c+1000000]++;
+        }
+        
+        for (int i=numList.length-1; i >= 0; i--) {
+            if (numList[i] > 0 && i >= 100000) {
+                bw.write((i-1000000) + "\n");
+                numList[i++]--;
+            } else if (numList[i] > 0 && i < 100000) {
+                bw.write((i-1000000) + "\n");
+                numList[i++]--;
+            }
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+        
+    }
+
+
+}
