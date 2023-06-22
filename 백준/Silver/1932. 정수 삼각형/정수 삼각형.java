47bf4c7c2584eed5253138a8666c1b2fcfe8cb2c
Nam Yooji <57939150+namyj@users.noreply.github.com>
2023-06-22 15:42:23 +0900

    [Silver I] Title: 정수 삼각형, Time: 292 ms, Memory: 27552 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -3,40 +3,44 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static ArrayList<int[]> numlist;
-    static ArrayList<int[]> D;
+    static int[][] numlist;
+    static int[][] D;
     
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        numlist = new ArrayList<>();
+        numlist = new int[501][501];
         
-        for (int i=0; i < n; i++) {
-            numlist.add(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray());           
-        }
-        
-        // 초기값 정의
-        // D[0] // 1 층
-        D = (ArrayList<int[]>) numlist.clone();
-        
-        for (int i=1; i < n; i++) {
-            int[] curLayer = numlist.get(i);
-            D.get(i)[0] = curLayer[0] + D.get(i-1)[0];
-            D.get(i)[curLayer.length-1] = curLayer[curLayer.length-1] + D.get(i-1)[curLayer.length-2];
-            for (int j=1; j < curLayer.length -1 ; j++) {
-                D.get(i)[j] = curLayer[j] + Math.max(D.get(i-1)[j], D.get(i-1)[j-1]);
+        for (int i=1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=1; j <= i; j++) {
+                numlist[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
+        // 초기값 정의
+        D = new int[501][501];
+        D[1][1] = numlist[1][1];
+        
+        for (int i=2; i <= n; i++) {
+            for (int j=1; j <= i; j++) { // i 층의 원소 개수 = i 개
+                D[i][j] = numlist[i][j] + Math.max(D[i-1][j], D[i-1][j-1]);
+            }
+        }
+        
+        // n 층에서 최대값 찾기
         int result = 0;
-        for (int i=0; i < D.get(n-1).length; i++ ) {
-            result = Math.max(result, D.get(n-1)[i]);            
+        for (int i=1; i <= n; i++) {
+            result = Math.max(result, D[n][i]);
         }
         
         bw.write(result + "");
+        
         bw.flush();
         bw.close();
         br.close();
 	}  
+    
 }
