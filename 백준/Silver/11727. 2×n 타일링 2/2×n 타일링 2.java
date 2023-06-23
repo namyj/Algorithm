9ace598f536676622fd7e672eaa30c7ad225e492
Nam Yooji <57939150+namyj@users.noreply.github.com>
2023-06-23 17:32:56 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] D;
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        
+        // 초기값 정의
+        D = new int[1001];
+        D[1] = 1;
+        D[2] = 3;
+        
+        for (int i=3; i <= n; i++) {
+            D[i] = (D[i-1] + D[i-2]*2) % 10007;
+        }
+        
+        System.out.println(D[n]);
+        
+        br.close();
+	}  
+    
+}
