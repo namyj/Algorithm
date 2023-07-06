9411a8ae1721b03322803c869ee9ae3858cd97ae
Nam Yooji <57939150+namyj@users.noreply.github.com>
2023-07-06 11:00:59 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int aVal = sc.nextInt();
+        int bVal = sc.nextInt();
+
+        System.out.println(aVal + bVal);
+    }
+}
