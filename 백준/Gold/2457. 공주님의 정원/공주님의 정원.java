a4b4be89fdaf0e96e88bc37337db5d48cbea44f6
Nam Yooji <57939150+namyj@users.noreply.github.com>
2023-07-07 18:04:33 +0900

    [Gold III] Title: 공주님의 정원, Time: 608 ms, Memory: 55940 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -6,7 +6,6 @@ import java.util.*;
 - 선택한 꽃의 최소 개수
 
 - 종료일이 12/1 까지 꽃 선택
-- 불가능하면 0 출력
 */
 public class Main {
     static int n;
@@ -35,29 +34,32 @@ public class Main {
             return o1[0] != o2[0]? o1[0] - o2[0] : o2[1] - o1[1];
         });
         
-        int cur_t = 301;
+        int cur_t = 301; // 현재 시간
         int result = 0;
         int idx = 0;
         
         while (cur_t < 1201) {
-            int next_t = cur_t;
+            int next_t = cur_t; // 다음 꽃의 지는 시간을 저장할 변수
             
             // 1. 현재 꽃 다음에 필 꽃 중에서 늦게 지는 꽃 찾기
-            for (int i=0; i < n; i++) {
+            for (int i=idx; i < n; i++) {
                 if (arr[i][0] <= cur_t && arr[i][1] > next_t ) {
                     next_t = arr[i][1];
+                    idx = i+1;
                 }
                     
             }
             
             // 2. 다음에 필 꽃이 있으면, 해당 꽃으로 cur 값 업데이트
             if (next_t != cur_t) {
+                // System.out.println(Arrays.toString(arr[idx-1]));
                 result++;
                 cur_t = next_t;
             } else {
                 result = 0;
                 break;
             }
+            
         }
         System.out.println(result);
 
