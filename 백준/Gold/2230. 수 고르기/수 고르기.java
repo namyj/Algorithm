ab79ff75740c0bec06a2d81144c6dae4f206bd5d
Nam Yooji <57939150+namyj@users.noreply.github.com>
2023-08-09 19:54:25 +0900

    [Gold V] Title: 수 고르기, Time: 364 ms, Memory: 27956 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,16 +1,14 @@
 import java.io.*;
 import java.util.*;
 
-/*
-N개의 정수 중 두 수의 차이가 M 이상이며 제일 작은 경우
-*/
-
 public class Main {
     static int n, m;
     static int[] arr;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         arr = new int[n];
@@ -20,27 +18,22 @@ public class Main {
         }
         Arrays.sort(arr);
         
+        int start = 0;
+        int end = 0;
         int min = Integer.MAX_VALUE;
-        for (int i=0; i < n; i++) {
-            int left = i+1;
-            int right = n-1;
+        
+        while (start < n && end < n) {
+            int sub = arr[end] - arr[start];
             
-            while (left <= right) {
-                int mid = (left+right)/2;
-                int sub = Math.abs(arr[i] - arr[mid]);
-                
-                if (sub >= m && min > sub) 
-                    min = sub;
-                
-                if (sub >= m) {
-                    // 차이가 크다 > 더 작은 수 탐색
-                    right = mid-1;
-                } else {
-                    // 차이가 작다 > 더 큰 수를 탐색
-                    left = mid+1;
-                }
+            if (sub >= m) {
+                min = Math.min(min, sub);
+                start++;
+            } else {
+                end++;
             }
         }
+        
         System.out.println(min);
     }
+    
 }
